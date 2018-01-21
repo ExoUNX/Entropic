4a1ca661cbfcdd9b95a204c016c7c3fded417671
Gaige Lama <gaigelama@gmail.com>
2018-01-21 17:07:47 -0500

    Split the ASCII charset to allow for semi-custom charset
    Signed-off-by: Gaige Lama <gaigelama@gmail.com>

--- src/Entropic.php
+++ src/Entropic.php
@@ -9,9 +9,18 @@ namespace ExoUNX\Entropic;
 class Entropic
 {
     /**
-     * Encoded in hexadecimal, Printable ASCII characters
+     * Printable ASCII characters
      */
-    private const DEFAULT_CHARSET = "2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e";
+
+    private const ASCII_SYMBOL = "!\\\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+    private const ASCII_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private const ASCII_LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+
+    private const ASCII_NUMERICAL = "0123456789";
+
+    private const DEFAULT_CHARSET = self::ASCII_SYMBOL . self::ASCII_LOWERCASE . self::ASCII_UPPERCASE . self::ASCII_NUMERICAL;
 
     /**
      * @param $length
@@ -64,6 +73,6 @@ class Entropic
 
     private function defaultCharset(): array
     {
-        return str_split(hex2bin(self::DEFAULT_CHARSET));
+        return str_split(self::DEFAULT_CHARSET);
     }
 }
